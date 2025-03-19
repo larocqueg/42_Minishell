50d121913e7a1bd70b4c6174c4a0851a631597af
therappha <therappha@gmail.com>
2025-03-19 12:37:42 +0000

    added pseudo shell

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 20:06:52 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/03/19 11:58:16 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/03/19 12:31:25 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 int	main(int ac, char *av[], char **envp)
 {
 	char	*prompt;
-	char	pwd[1024];
+	char	*pwd;
 
+	pwd = malloc(sizeof(char) * 4096);
 	(void)ac;
 	(void)av;
 	(void)envp;
 
-	getcwd(pwd, 1024);
-	strcat(pwd, " < $ ");
-	prompt = readline(pwd);
+	getcwd(pwd, 4096);
+	pwd = strrchr(pwd, '/');
+	strcat(pwd++, " <$ ");
+
+	while (1)
+	{
+		printf("\033[0;31m%s\033[0m", pwd);
+		prompt = readline(NULL);
+	}
 }
