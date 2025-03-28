b1c9fe6dd31767617a7b72082a5ea8ea934dd92d
larocqueg <portugalgabriel2021@gmail.com>
2025-03-28 15:48:17 +0000

    tokenize malloc redone

--- srcs/parsing/tokenize.c
+++ srcs/parsing/tokenize.c
@@ -29,48 +29,59 @@ static t_type	get_token_type(char *token)
 	return (WORD);
 }
 
-static int	extract_quotes(char *prompt, int i, char *token, int *j)
+static int	extract_quotes(char *prompt, int i, char **token)
 {
+	int		start;
     char	quote;
 
+	start = i;
     quote = prompt[i];
-    token[(*j)++] = prompt[i++]; // Add the opening quote
-    while (prompt[i] && prompt[i] != quote)
-        token[(*j)++] = prompt[i++]; // Add characters inside the quotes
-    if (prompt[i] == quote)
-        token[(*j)++] = prompt[i++]; // Add the closing quote
-    return (i);
+	i++;
+	while(prompt[i] && prompt[i] != quote)
+		i++;
+	if (prompt[i] == quote)
+		*token = ft_strndupmod(prompt, start, i++);
+	else
+		*token = ft_strndupmod(prompt, start, i - 1);
+	return (i);
+}
+
+static int	extract_word(char *prompt, int i, char **token)
+{
+	int	start;
+
+	start = i;
+	while (prompt[i] && !is_space(prompt[i]) && !is_operator(prompt[i])
+			&& prompt[i] != '\'' && prompt[i] != '"')
+		i++;
+	*token = ft_strndupmod(prompt, start, i - 1);
+	if (prompt[i] == '\'' || prompt[i] == '"')
+		i--;
+	return (i);
 }
 
 int extract_token(char *prompt, int i, t_token **tokens)
 {
-	int j;
-	char *token;
+	int		start;
+	char	*token;
 	t_token *new_token;
 
-	j = 0;
-	token = malloc(sizeof(char) * 4096);
-	if (!token)
-		return (i);
-
+	start = 0;
 	while (prompt[i] && !is_space(prompt[i]))
 	{
 		if (is_operator(prompt[i]))
 		{
-			if (j > 0)
-				break;
-			token[j++] = prompt[i++];
-			if (prompt[i] == token[j - 1])
-				token[j++] = prompt[i++];
-			break;
+			start = i;
+			i++;
+			if (prompt[i] == prompt[i - 1])
+				i++;
+			token = ft_strndupmod(prompt, start, i);
 		}
 		else if (prompt[i] == '\'' || prompt[i] == '"')
-			i = extract_quotes(prompt, i, token, &j);
+			i = extract_quotes(prompt, i, &token);
 		else
-			token[j++] = prompt[i++];
+			i = extract_word(prompt, i, &token);
 	}
-
-	token[j] = '\0';
 	new_token = ft_tokennew(token, get_token_type(token));
 	ft_token_addback(tokens, new_token);
 	free(token);
