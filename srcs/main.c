873958da0f030df0caa27abc1b11c0331fcbe6d7
therappha <therappha@gmail.com>
2025-04-17 17:29:59 +0100

    added shlvl'

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 20:44:47 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/04/15 21:40:45 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:29:48 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,31 @@ void	ft_sh_init(t_shell *sh, char **envp)
 	sh->heredoc_pipes = NULL;
 	sh->heredoc_count = 0;
 }
+void	ft_get_shlvl(t_shell *sh)
+{
+	char		**env;
+	long long	shlvl;
+	char		*temp;
+	char 		*itoa;
+
+	env = sh->envp;
+	temp = ft_get_env("SHLVL", sh->envp);
+	if (!temp)
+	{
+		create_export("SHLVL=1", sh);
+		return ;
+	}
+	shlvl = ft_atoll(temp);
+	if (shlvl < 0)
+		shlvl = 0;
+	else if (shlvl > 1000)
+		shlvl = 0;
+	itoa = ft_itoa(shlvl + 1);
+	temp = ft_strjoin("SHLVL=", itoa);
+	create_export(temp, sh);
+	free(temp);
+	free(itoa);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -50,6 +75,7 @@ int	main(int argc, char **argv, char **envp)
 	if (argc > 1)
 		return (0);
 	ft_sh_init(&sh, envp);
+	ft_get_shlvl(&sh);
 	start_cli(&sh);
 	free_envp(&sh);
 	return (0);
