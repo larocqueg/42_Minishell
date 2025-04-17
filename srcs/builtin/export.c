c727b131d629044a44c3aa4c41fd794d110223d7
therappha <therappha@gmail.com>
2025-04-17 18:36:53 +0100

    export

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,35 +6,13 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 19:00:07 by gde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/17 18:32:44 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/04/17 18:36:10 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	put_export(char *str)
-{
-	int		i;
-	bool	equal;
 
-	equal = false;
-	i = 0;
-	ft_printf("declare -x ");
-	while (str[i])
-	{
-		if (str[i] == '=' && !equal)
-		{
-			equal = true;
-			write(1, &str[i++], 1);
-			write(1, "\"", 1);
-		}
-		else
-			write(1, &str[i++], 1);
-	}
-	if (equal)
-		write(1, "\"", 1);
-	write(1, "\n", 1);
-}
 
 void	print_export(t_shell *sh)
 {
@@ -127,87 +105,6 @@ void	create_export(char *str, t_shell *sh)
 	}
 	free(no_equal);
 	free(no_quotes);
-	free(var_name);
-	ft_exit_status(0, 1, 0);
-}
-
-int	is_append_var(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!ft_isalpha(str[0]) && str[0] != '_')
-		return (0);
-	while (ft_isalnum(str[i]) || str[i] == '_')
-	{
-		i++;
-	}
-	if (str[i] == '+')
-		i++;
-	if (str[i] == '=')
-		return (1);
-	else
-		return (0);
-}
-
-void	create_var(char *var_name, char *var, t_shell *sh)
-{
-	char	*temp;
-	char	*value;
-
-	value = (ft_strjoin(var_name, "="));
-	if (!value)
-		return ;
-	temp = ft_strjoin(value, var + ft_strlen_tochar(var, '=') + 1);
-	if (!temp)
-	{
-		free(value);
-		return ;
-	}
-	create_export(temp, sh);
-	free(value);
-	free(temp);
-	free(var_name);
-}
-
-void	append_var(char *var_name, char *var, char *temp, t_shell *sh)
-{
-	char	*value;
-
-	value = ft_strjoin(temp, var + ft_strlen_tochar(var, '=') + 1);
-	if (!value)
-		return ;
-	temp = ft_strjoin(var_name, "=");
-	if (!temp)
-	{
-		free(value);
-		return ;
-	}
-	free(var_name);
-	var_name = ft_strjoin(temp, value);
-	if (!var_name)
-		return ;
-	create_export(var_name, sh);
-	free(temp);
-	free(value);
-}
-
-void	do_append(char *var, t_shell *sh)
-{
-	int		i;
-	char	*temp;
-	char	*var_name;
-	char	*value;
-
-	i = 0;
-	var_name = ft_strndupmod(var, 0, ft_strlen_tochar(var, '+') - 1);
-	if (!var_name)
-		return ;
-	temp = ft_get_env(var_name, sh->envp);
-	if (!temp)
-		create_var(var_name, var, sh);
-	else
-		append_var(var_name, var, temp, sh);
 	free(var_name);
 	ft_exit_status(0, 1, 0);
 }
